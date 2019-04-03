21b745f594a444a5bb9ca94578d2dc75d0cb1b55
tynanseltzer <tynanseltzer@gmail.com>
2019-04-02 22:40:05 -0400

    working version, bit slow, wrong algo maybe?

--- lissa.py
+++ lissa.py
@@ -4,12 +4,14 @@
 # hess(point) gives hessian at point
 import numpy as np
 import pandas as pd
+from scipy.optimize import minimize
+from scipy.optimize import line_search
 def lissa(x_start, grad, hess, data, NUM_ITERS, S1, S2):
     x_curr = x_start
     for t in range(1, NUM_ITERS + 1):
         step_list = []
         for i in range(1, S1 + 1):
-            step_list.append(grad(x_curr, data))
+            step_list.append(np.zeros(data.shape[1]-1))
             for j in range(1, S2 + 1):
                 idx = np.random.choice(data.shape[0])
                 p = data[idx][:-1]
@@ -18,8 +20,8 @@ def lissa(x_start, grad, hess, data, NUM_ITERS, S1, S2):
                 step_list[-1] = np.dot(step_list[-1].T,  (np.eye(data.shape[1]-1) - hess(x_curr, p)).T) + grad(x_curr, data)
         total = sum(step_list)
         avg = total / S1
-
-        x_curr -= avg
+        x_curr -= line_search(loss, logistic_gradient, xk= x_curr, pk=-avg, args=([data]))[0] * avg
+        print(t)
         print(x_curr)
         print(loss(x_curr, data))
     return x_curr
@@ -28,7 +30,8 @@ def sigmoid(z):
     return 1/ (1 + np.exp(-z))
 
 def zee(x_curr, datapoint):
-    return np.dot(x_curr, datapoint)
+    foo = np.dot(x_curr, datapoint)
+    return foo
 
 # From https://stats.stackexchange.com/questions/68391/hessian-of-logistic-function
 def logistic_gradient(x_curr, data):
@@ -37,6 +40,7 @@ def logistic_gradient(x_curr, data):
 
 def logistic_hessian(x_curr, data_point):
     data_point = np.atleast_2d(data_point)
+    x_curr = np.atleast_2d(x_curr)
     bar =  np.dot(data_point.T, data_point)*sigmoid(np.dot(x_curr, data_point.T))*(1-sigmoid(np.dot(x_curr, data_point.T)))
     return bar
 
@@ -44,9 +48,12 @@ def loss(x_curr, data):
     loss = 0
     for i in range(data.shape[0]):
         d = data[i][:-1]
-        loss += -(data[i][-1] * np.log(sigmoid(zee(x_curr, d))) + (1- data[i][-1]) * (1- np.log(sigmoid(zee(x_curr, d)))))
+        loss += -(data[i][-1] * np.log(sigmoid(zee(x_curr, d))) + (1- data[i][-1]) * np.log(1-(sigmoid(zee(x_curr, d)))))
     return loss
+
 df = pd.read_csv("banknote.txt", header=None).values
 data = np.insert(df, -1, np.ones(df.shape[0]), axis=1)
+print(minimize(loss, np.zeros(data.shape[1] - 1), args=(data)))
+
 print(data[0])
-print(lissa(np.zeros(data.shape[1] - 1), logistic_gradient, logistic_hessian, data, 100, 200, 3))
+print(lissa([-5.0042507,  -3.02215979, -3.4448039,  -1.22954548,  2.04449799], logistic_gradient, logistic_hessian, data, 20000, 1000, 4))
